f438a40dad06fdcfa0bf3e37e57a0a90729b4a56
Mircea Scarlatescu <scarlatescu.mircea8@gmail.com>
2023-11-19 00:57:54 +0200

    Switched from function to method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
@@ -8,10 +15,7 @@ fn main() {
         width: 30,
         height: 50,
     };
+    
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
